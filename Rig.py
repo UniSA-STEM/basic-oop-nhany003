85d0fe3a301959d2f23a70bf477f587fa26a89a2
NNhanga <128450664+NNhanga@users.noreply.github.com>
2025-10-17 13:43:34 +1100

    Started first part of Rig class

--- Rig.py
+++ Rig.py
@@ -6,3 +6,17 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+
+class Rig:
+    def __init__(self, name='', damage_counter=0, broken_state=False, storage=None, data_spike=2, removable_drive=1,
+                 upgrade_level=0):
+        if storage is None:
+            storage = []
+        self.name = name
+        self.damage_counter = damage_counter
+        self.broken_state = broken_state
+        self.storage = storage
+        self.data_spike = data_spike
+        self.removable_drive = removable_drive
+        self.upgrade_level = upgrade_level
