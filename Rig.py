b77c1f2cf98299e550fe4ac23c8fbcef0bb2f05c
NNhanga <128450664+NNhanga@users.noreply.github.com>
2025-10-24 00:47:12 +1100

    Finished Rig.py

--- Rig.py
+++ Rig.py
@@ -8,15 +8,14 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
 
+import random
 class Rig:
-    def __init__(self, name='', damage_counter=0, broken_state=False, storage=None, data_spike=2, removable_drive=1,
+    def __init__(self, name='', damage_counter=0, broken_state=False, data_spike=2, removable_drive=1,
                  upgrade_level=0):
-        if storage is None:
-            storage = []
         self.name = name
         self.damage_counter = damage_counter
         self.broken_state = broken_state
-        self.storage = storage
+        self.storage = ['Data Spike', 'Data Spike', 'Removable Drive']
         self.data_spike = data_spike
         self.removable_drive = removable_drive
         self.upgrade_level = upgrade_level
@@ -28,4 +27,55 @@ class Rig:
         if self.damage_counter >= 2:
             self.broken_state = True
             print('Rig is broken!')
+
+    def generate_asset(self):
+        possible_assets = ['CryptoToken', 'Hardware Patch', 'Data Spike', 'Removable Drive']
+        asset = random.choice(possible_assets)
+        print(f'Generated: {asset}')
+        return asset
+
+    def store_asset(self,asset):
+        self.storage.append(asset)
+
+    def transfer_asset(self,asset):
+        if asset in self.storage:
+            self.storage.remove(asset)
+            return asset
+        else:
+            print(f'{asset} not found in storage')
+            return None
+
+    def repair(self):
+        if self.damage_counter >= 2:
+            self.damage_counter = 0
+            self.broken_state = False
+        else:
+            print('No repair needed')
+
+    def upgrade(self):
+        self.upgrade_level += 1
+
+    def get_condition(self):
+        if self.broken_state:
+            condition = 'broken'
+        elif self.damage_counter == 0:
+            condition = 'pristine'
+        else:
+            condition = 'damaged'
+        return f'{condition} (Level {self.upgrade_level})'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
